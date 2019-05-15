08af76d5487ed45b44a13f29caf6a6d8b8bb041f
Cesar Mejia <crsmejia93@gmail.com>
2019-05-15 16:57:31 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot306/DirectorRepository.java
@@ -0,0 +1,5 @@
+package com.example.springboot306;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DirectorRepository extends CrudRepository<Director, Long> {}
---
+++ src/main/java/com/example/springboot306/MovieRepository.java
@@ -0,0 +1,5 @@
+package com.example.springboot306;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MovieRepository extends CrudRepository<Movie, Long> {}
